837cbd35258cf2c14c95f32b2c049ef407f43f3d
Riccardo Pisano <riccardopisano02@gmail.com>
2025-08-20 16:00:30 +0200

    Implementato fortify, quasi ultimata la pagina homepage con estetica annessa, scelto font e colori e dio cane

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- FONTS --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    {{-- VITE --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- STYLE --}}
     <link rel="stylesheet" href="style.css">
     <title>PokeTrainer</title>
 </head>
-<body>
-    <x-nav/>
 
-    <div class="min-vh-100">
-    {{$slot}}
-    </div>
+<body>
+    <x-nav />
+
+    {{-- <div class="min-vh-100"> --}}
+    {{ $slot }}
+    {{-- </div> --}}
 
     {{-- <x-footer/> --}}
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,38 @@
 <x-layout>
-    <div class="container-fluid header">
-        <div class="">
+    <div class="container-fluid header my-5 ">
+        <div class="header-content" id="text-box">
 
         </div>
     </div>
+    <div class=" content">
+        <div class="content-content">
+            <h1 class=" text-center">Alessandro pederiva Gay</h1>
+        </div>
+    </div>
+    <script>
+        const text =
+            `Salve allenatore, ho bisogno del tuo aiuto per sconfiggere Giratina!
+            Registrati per scoprire come fare!
+            `;
+        const box = document.getElementById("text-box");
+
+        let i = 0;
+
+        function typeWriter() {
+            if (i < text.length) {
+                if (text[i] === "\n") {
+                    box.innerHTML += "<br>";
+                } else {
+                    box.innerHTML += text[i];
+                }
+
+                i++;
+                setTimeout(typeWriter, 50); // velocità
+            }
+
+        }
+
+
+        typeWriter();
+    </script>
 </x-layout>
